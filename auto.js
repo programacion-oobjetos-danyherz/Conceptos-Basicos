907a5c5c533ff0e1fe59a3bdca85ca7cc4405af8
20155354 HERNANDEZ ASCENCIO DANIEL <20155354@TELEMATICA.UCOL.MX>
2019-02-07 19:16:00 -0600

    3.0

--- auto.js
+++ auto.js
@@ -1,5 +1,5 @@
-class auto{
-    constructor (marca, modelo, color, kilometraje){
+export default class auto{
+    constructor (marca, modelo, color = "blanco", kilometraje = 0){
         this.marca = marca;
         this.modelo = modelo;
         this.color = color;
@@ -7,5 +7,26 @@ class auto{
         this.estado = "Apagado";
     }
     
-     
+    mostrarEstado(){
+        console.log(`El auto ${this.marca} ${this.color} del año ${this.modelo} tiene un kilometraje de ${this.kilometraje} kms y esta ${this.estado}`)
+    }
+    encender(){
+        this.estado = "Encendido";
+        this.mostrarEstado();
+    }
+    apagar(){
+        this.estado = "Apagado";
+        this.mostrarEstado()
+    }
+
+    avanzar(velocidad, tiempo){
+        if(this.estado === "Apagado"){
+            console.log("El auto no puede avanzar por que esta apagado")
+        }
+        else{
+            let distancia = velocidad + tiempo;
+            this.kilometraje = this.kilometraje + distancia;
+            this.mostrarEstado();
+        }
+    }
 }
--- main.js
+++ main.js
@@ -1,2 +1,13 @@
-console.log("Hola Mundo");
+import Auto from "./auto.js";
 
+let auto1 = new Auto("ford",2000,"rojo",100);
+let auto2 = new Auto("Nissan",2010,"azul");
+let auto3 = new Auto("Toyota",2014);
+auto1.encender();
+auto1.avanzar(50, 1.5)
+auto1.apagar();
+auto1.avanzar(50, 1.5)
+
+console.log(`Kilometraje ${auto1.kilometraje}`);
+auto1.kilometraje = 5;
+console.log(`Kilometraje ${auto1.kilometraje}`);
